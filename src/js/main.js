9f4c15c78bffc7ae3f4aa38a86257d123aa9aa18
Kaung Satt <kaungsatt.2k1@gmail.com>
2024-04-10 11:43:18 +0630

    completed

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,27 @@
-import '../scss/style.scss';
+import "../scss/style.scss";
+import $ from "jquery";
+import "slick-carousel";
+
+$(".gallery__slider").slick({
+  centerMode: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 5,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        centerMode: true,
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
