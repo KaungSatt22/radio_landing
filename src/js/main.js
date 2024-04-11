cd0f6289773195f360df38f4f597953fb756a884
Khinesuwai_01 <khinesuwai9812@gmail.com>
2024-04-11 13:40:25 +0630

    modified

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
 import '../scss/style.scss';
+
+let btn = document.querySelector(".hamburger");
+let navbar = document.querySelector(".l-header__menu ul");
+btn.addEventListener("click", ()=> {
+    navbar.classList.toggle("show")
+})
